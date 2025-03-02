38758e4a191c1d1f8035ff74ef63aea703632517
Julia Doyle <Doylej7457@clarkstate.edu>
2025-03-02 00:58:36 -0500

    Finished Work

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -13,3 +13,15 @@
 #13
 #14
 #FizzBuzz
+
+
+for i in range(25):
+    #Keep in THIS order, or else it BREAK
+    if i%3==0 and i%5==0:
+        print("Fizz Buzz")
+    elif i% 3 == 0:
+        print("Fizz")
+    elif i% 5 == 0:
+        print("Buzz")
+    else:
+        print (i)
